1da94fd112b9c3361c167860aa6f76a1b82c69e2
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-09-27 13:31:56 +0530

    commit 3

---
+++ p11.py
@@ -0,0 +1,9 @@
+dict={'Name':'Zara', 'Age':7, 'Class':'First'}
+print("dict['Name']: ",dict['Name'])
+print("dict['Age']: ",dict['Age'])
+
+dict['Age']=8
+dict['School']="DPS School"
+
+print("dict['Age']: ",dict['Age'])
+print("dict['School']: ",dict['School'])
